3f1632e61c0bae5e366b72d4238594fccca577ce
Bobby Towers <porkostomus@mail.com>
2022-10-12 08:21:05 -0700

    load, decode, play samples

--- src/main/phase_vocoder/app.cljs
+++ src/main/phase_vocoder/app.cljs
@@ -1,13 +1,49 @@
 (ns phase-vocoder.app
-  (:require [reagent.dom :as rdom]))
+  (:require 
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
+   [goog.object :as o]))
 
 (defn square-root
   [x]
   (.sqrt js/Math x))
 
+(defonce audio-context (js/AudioContext.))
+
+(defonce current-sample (r/atom nil))
+
+(defn file-upload []
+  [:input#input
+   {:type      "file"
+    :on-change (fn [e]
+                 (let [dom    (o/get e "target")
+                       file   (o/getValueByKeys dom #js ["files" 0])
+                       reader (js/FileReader.)]
+                   (.readAsArrayBuffer reader file)
+                   (set! (.-onload reader)
+                         (fn [buff] (->
+                                 (.decodeAudioData audio-context (-> buff .-target .-result))
+                                 (.then #(reset! current-sample %)))))))}])
+
+(defn buffer-source [context buffer]
+  (let [source (.createBufferSource context)
+        gain (.createGain context)]
+    (.resume context)
+    (set! (.-buffer source) buffer)
+    (.setValueAtTime (.-gain gain) 0.1 (.-currentTime context))
+    (.connect source gain)
+    (.connect gain (.-destination context))
+    (.start source (.-currentTime context))
+    source))
+
 (defn app []
   [:div#app
-   [:h1 "phase-vocoder"]])
+   [:h1 "phase-vocoder"]
+   [file-upload]
+
+   [:button
+    {:on-click (fn [] (buffer-source audio-context @current-sample))}
+    "Play"]])
 
 (defn render []
   (rdom/render [app]
